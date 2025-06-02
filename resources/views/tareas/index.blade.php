92eeaf2d32f0e3ccf6f51e09651f1d72bb063ae7
“AleDavFernandez” <alejandrodavfernandez@gmail.com>
2025-06-02 19:47:02 -0300

    02-06

--- resources/views/tareas/index.blade.php
+++ resources/views/tareas/index.blade.php
@@ -1,25 +1,35 @@
-<h1>Lista de Tareas</h1>
-
-
-<form action="/tareas" method="POST">
-    @csrf
-    <input type="text" name="titulo" placeholder="Nueva tarea">
-    <button type="submit">Agregar</button>
-</form>
-
-
-<ul>
-    @foreach($tareas as $tarea)
-        <li>
-            {{ $tarea->titulo }} -
-            <a href="/tareas/{{ $tarea->id }}/edit">Editar</a>
-            <form action="/tareas/{{ $tarea->id }}" method="POST" style="display:inline;">
-                @csrf
-                @method('DELETE')
-                <button type="submit">Eliminar</button>
-            </form>
-        </li>
-
-
-    @endforeach
-</ul>
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($tareas as $tarea)
+                                <tr>
+                                    <td>{{ $tarea->id }}</td>
+                                    <td>{{ $tarea->nombre }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
