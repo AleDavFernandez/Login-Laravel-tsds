096a45fdc4f738bb16083cac42e0a8376efa0f7b
“aledavfernandez” <alejandrodavfernandez@gmail.com>
2025-07-11 22:28:37 -0300

    edit-update

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,10 @@ Route::get('/tareas/create', [App\Http\Controllers\TareaController::class, 'crea
 Route::post('/tareas', [App\Http\Controllers\TareaController::class, 'store'])
     ->name('tareas.store');
 
+Route::get('/tareas/{tarea}/edit',[App\Http\Controllers\TareaController::class, 'edit'])->name('tareas.edit');
+
+Route::put('/tareas/{tarea}',[App\Http\Controllers\TareaController::class, 'update'])->name('tareas.update');
+
 Route::delete('/tareas/{tarea}', [App\Http\Controllers\TareaController::class, 'destroy'])
     ->name('tareas.destroy');
 
